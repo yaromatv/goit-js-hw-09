4e97c0433f9e59da57252856291279ab3bc125d6
yaromatv <122043732+yaromatv@users.noreply.github.com>
2023-06-11 10:33:00 +0000

    Deploying to gh-pages from @ yaromatv/goit-js-hw-09@ed66d3ec8cb99ec878602a43e3ddc7e7a400b1ca 🚀

---
+++ 01-color-switcher.65619816.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){o=setInterval(r,1e3),e.disabled=!0})),n.addEventListener("click",(function(){if(!o)return;clearInterval(o),e.disabled=!1}));var o=null;function r(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}}();
+//# sourceMappingURL=01-color-switcher.65619816.js.map
